e8083b6d29fe2bdbe3b71e311ab6fd444e10c4f8
AkaniX3 <parthrajkondawar@gmail.com>
2024-12-25 16:44:51 +0530

    route update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,5 +7,8 @@ import reviewRoute from "./review.route.js"
 const router = express.Router()
 
 router.use("/user", userRoute)
+router.use("/person", personRoute)
+router.use("/:mediaType", mediaRoute)
+router.use("/reviews", reviewRoute)
 
 export default router;
